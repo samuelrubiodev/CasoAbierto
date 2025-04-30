e14f75515f1dfbe71709bb54698a58275615e629
Darkenight07 <samueldynamo11@gmail.com>
2025-04-30 22:57:38 +0200

    fix: Player controller as it sometimes could not jump and sit properly. #1 #7

--- Assets/ModularFirstPersonController/FirstPersonController/FirstPersonController.cs
+++ Assets/ModularFirstPersonController/FirstPersonController/FirstPersonController.cs
@@ -16,6 +16,7 @@ using UnityEngine.UI;
 
 public class FirstPersonController : MonoBehaviour
 {
+    public float test = 0f;
     private Rigidbody rb;
 
     #region Camera Movement Variables
@@ -381,18 +382,20 @@ public class FirstPersonController : MonoBehaviour
 
                     GameObject gameObject = this.gameObject;
 
-                    gameObject.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+                    gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
 
-                    gameObject.transform.position = new Vector3(0.15f, 2.01f, -7.903907f);
-
-                    this.playerCanMove = false;
-
-                    GameObject chair = hit.collider.gameObject;
-                    chair.GetComponent<BoxCollider>().enabled = false;
+                    GameObject chairObject = hit.collider.gameObject;
+                    chairObject.GetComponent<BoxCollider>().enabled = false;
 
                     GameObject table = GameObject.Find("Table");
                     table.GetComponent<BoxCollider>().enabled = false;
 
+                    this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+                    Vector3 chairPosition = chairObject.transform.position;
+                    gameObject.transform.position = new Vector3(chairPosition.x + 0.5f, 1.16f, chairPosition.z + 0.3f);
+
+                    this.playerCanMove = false;
                 }
             }
         }
@@ -407,88 +410,73 @@ public class FirstPersonController : MonoBehaviour
         {
             if (hit.collider.CompareTag("Door"))
             {
-                GUIStyle customStyle = new GUIStyle(GUI.skin.label);
-                customStyle.normal.textColor = Color.white; // Color del texto blanco
-                customStyle.fontSize = 30; // Tamaño de la fuente
-                customStyle.alignment = TextAnchor.MiddleCenter; // Centrar el texto
+                GUIStyle customStyle = new (GUI.skin.label);
+                customStyle.normal.textColor = Color.white; 
+                customStyle.fontSize = 30; 
+                customStyle.alignment = TextAnchor.MiddleCenter; 
 
-                // Crear un contenido con borde
                 string message = "Presiona E para abrir/cerrar la puerta";
 
-                // Obtener el centro de la pantalla
                 float screenCenterX = Screen.width / 2;
                 float screenCenterY = Screen.height / 2;
 
-                // Dimensiones del label
                 float labelWidth = 500;
                 float labelHeight = 50;
 
-                // Crear el rectángulo para el borde
-                Rect borderRect = new Rect(
-                    screenCenterX - labelWidth / 2 - 2, // -2 para el borde
-                    screenCenterY - labelHeight / 2 - 2, // -2 para el borde
-                    labelWidth + 4, // +4 para el borde
-                    labelHeight + 4 // +4 para el borde
+                Rect borderRect = new (
+                    screenCenterX - labelWidth / 2 - 2, 
+                    screenCenterY - labelHeight / 2 - 2, 
+                    labelWidth + 4, 
+                    labelHeight + 4 
                 );
 
-                // Dibujar el borde
-                GUI.color = new Color(0.5f, 0.5f, 0.5f, 1f); // Color gris
+                GUI.color = new Color(0.5f, 0.5f, 0.5f, 1f); 
                 GUI.Box(borderRect, "");
 
-                // Crear el rectángulo para el texto
-                Rect labelRect = new Rect(
+                Rect labelRect = new (
                     screenCenterX - labelWidth / 2,
                     screenCenterY - labelHeight / 2,
                     labelWidth,
                     labelHeight
                 );
 
-                // Dibujar el texto
-                GUI.color = Color.white; // Restaurar el color para el texto
+                GUI.color = Color.white; 
                 GUI.Label(labelRect, message, customStyle);
             }
 
-
             if (hit.collider.CompareTag("Chair"))
             {
-                GUIStyle customStyle = new GUIStyle(GUI.skin.label);
-                customStyle.normal.textColor = Color.white; // Color del texto blanco
-                customStyle.fontSize = 30; // Tamaño de la fuente
-                customStyle.alignment = TextAnchor.MiddleCenter; // Centrar el texto
+                GUIStyle customStyle = new (GUI.skin.label);
+                customStyle.normal.textColor = Color.white;
+                customStyle.fontSize = 30; 
+                customStyle.alignment = TextAnchor.MiddleCenter; 
 
-                // Crear un contenido con borde
                 string message = "Presiona E para sentarse";
 
-                // Obtener el centro de la pantalla
                 float screenCenterX = Screen.width / 2;
                 float screenCenterY = Screen.height / 2;
 
-                // Dimensiones del label
                 float labelWidth = 500;
                 float labelHeight = 50;
 
-                // Crear el rectángulo para el borde
-                Rect borderRect = new Rect(
-                    screenCenterX - labelWidth / 2 - 2, // -2 para el borde
-                    screenCenterY - labelHeight / 2 - 2, // -2 para el borde
-                    labelWidth + 4, // +4 para el borde
-                    labelHeight + 4 // +4 para el borde
+                Rect borderRect = new (
+                    screenCenterX - labelWidth / 2 - 2, 
+                    screenCenterY - labelHeight / 2 - 2, 
+                    labelWidth + 4, 
+                    labelHeight + 4 
                 );
 
-                // Dibujar el borde
-                GUI.color = new Color(0.5f, 0.5f, 0.5f, 1f); // Color gris
+                GUI.color = new Color(0.5f, 0.5f, 0.5f, 1f); 
                 GUI.Box(borderRect, "");
 
-                // Crear el rectángulo para el texto
-                Rect labelRect = new Rect(
+                Rect labelRect = new (
                     screenCenterX - labelWidth / 2,
                     screenCenterY - labelHeight / 2,
                     labelWidth,
                     labelHeight
                 );
 
-                // Dibujar el texto
-                GUI.color = Color.white; // Restaurar el color para el texto
+                GUI.color = Color.white;
                 GUI.Label(labelRect, message, customStyle);
             }
         }
@@ -501,7 +489,7 @@ public class FirstPersonController : MonoBehaviour
         if (playerCanMove)
         {
             // Calculate how fast we should be moving
-            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            Vector3 targetVelocity = new (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
             // Checks if player is walking and isGrounded
             // Will allow head bob
@@ -576,7 +564,7 @@ public class FirstPersonController : MonoBehaviour
     {
         Vector3 origin = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * .5f), transform.position.z);
         Vector3 direction = transform.TransformDirection(Vector3.down);
-        float distance = .75f;
+        float distance = .85f;
 
         if (Physics.Raycast(origin, direction, out RaycastHit hit, distance))
         {
@@ -672,199 +660,6 @@ public class FirstPersonController : MonoBehaviour
     {
         fpc = (FirstPersonController)target;
         SerFPC = new SerializedObject(fpc);
-    }
-
-    public override void OnInspectorGUI()
-    {
-        SerFPC.Update();
-
-        EditorGUILayout.Space();
-        GUILayout.Label("Modular First Person Controller", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 16 });
-        GUILayout.Label("By Jess Case", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Normal, fontSize = 12 });
-        GUILayout.Label("version 1.0.1", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Normal, fontSize = 12 });
-        EditorGUILayout.Space();
-
-        #region Camera Setup
-
-        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-        GUILayout.Label("Camera Setup", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 13 }, GUILayout.ExpandWidth(true));
-        EditorGUILayout.Space();
-
-        fpc.playerCamera = (Camera)EditorGUILayout.ObjectField(new GUIContent("Camera", "Camera attached to the controller."), fpc.playerCamera, typeof(Camera), true);
-        fpc.fov = EditorGUILayout.Slider(new GUIContent("Field of View", "The camera’s view angle. Changes the player camera directly."), fpc.fov, fpc.zoomFOV, 179f);
-        fpc.cameraCanMove = EditorGUILayout.ToggleLeft(new GUIContent("Enable Camera Rotation", "Determines if the camera is allowed to move."), fpc.cameraCanMove);
-
-        GUI.enabled = fpc.cameraCanMove;
-        fpc.invertCamera = EditorGUILayout.ToggleLeft(new GUIContent("Invert Camera Rotation", "Inverts the up and down movement of the camera."), fpc.invertCamera);
-        fpc.mouseSensitivity = EditorGUILayout.Slider(new GUIContent("Look Sensitivity", "Determines how sensitive the mouse movement is."), fpc.mouseSensitivity, .1f, 10f);
-        fpc.maxLookAngle = EditorGUILayout.Slider(new GUIContent("Max Look Angle", "Determines the max and min angle the player camera is able to look."), fpc.maxLookAngle, 40, 90);
-        GUI.enabled = true;
-
-        fpc.lockCursor = EditorGUILayout.ToggleLeft(new GUIContent("Lock and Hide Cursor", "Turns off the cursor visibility and locks it to the middle of the screen."), fpc.lockCursor);
-
-        fpc.crosshair = EditorGUILayout.ToggleLeft(new GUIContent("Auto Crosshair", "Determines if the basic crosshair will be turned on, and sets is to the center of the screen."), fpc.crosshair);
-
-        // Only displays crosshair options if crosshair is enabled
-        if(fpc.crosshair) 
-        { 
-            EditorGUI.indentLevel++; 
-            EditorGUILayout.BeginHorizontal(); 
-            EditorGUILayout.PrefixLabel(new GUIContent("Crosshair Image", "Sprite to use as the crosshair.")); 
-            fpc.crosshairImage = (Sprite)EditorGUILayout.ObjectField(fpc.crosshairImage, typeof(Sprite), false);
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-            fpc.crosshairColor = EditorGUILayout.ColorField(new GUIContent("Crosshair Color", "Determines the color of the crosshair."), fpc.crosshairColor);
-            EditorGUILayout.EndHorizontal();
-            EditorGUI.indentLevel--; 
-        }
-
-        EditorGUILayout.Space();
-
-        #region Camera Zoom Setup
-
-        GUILayout.Label("Zoom", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleLeft, fontStyle = FontStyle.Bold, fontSize = 13 }, GUILayout.ExpandWidth(true));
-
-        fpc.enableZoom = EditorGUILayout.ToggleLeft(new GUIContent("Enable Zoom", "Determines if the player is able to zoom in while playing."), fpc.enableZoom);
-
-        GUI.enabled = fpc.enableZoom;
-        fpc.holdToZoom = EditorGUILayout.ToggleLeft(new GUIContent("Hold to Zoom", "Requires the player to hold the zoom key instead if pressing to zoom and unzoom."), fpc.holdToZoom);
-        fpc.zoomKey = (KeyCode)EditorGUILayout.EnumPopup(new GUIContent("Zoom Key", "Determines what key is used to zoom."), fpc.zoomKey);
-        fpc.zoomFOV = EditorGUILayout.Slider(new GUIContent("Zoom FOV", "Determines the field of view the camera zooms to."), fpc.zoomFOV, .1f, fpc.fov);
-        fpc.zoomStepTime = EditorGUILayout.Slider(new GUIContent("Step Time", "Determines how fast the FOV transitions while zooming in."), fpc.zoomStepTime, .1f, 10f);
-        GUI.enabled = true;
-
-        #endregion
-
-        #endregion
-
-        #region Movement Setup
-
-        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-        GUILayout.Label("Movement Setup", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 13 }, GUILayout.ExpandWidth(true));
-        EditorGUILayout.Space();
-
-        fpc.playerCanMove = EditorGUILayout.ToggleLeft(new GUIContent("Enable Player Movement", "Determines if the player is allowed to move."), fpc.playerCanMove);
-
-        GUI.enabled = fpc.playerCanMove;
-        fpc.walkSpeed = EditorGUILayout.Slider(new GUIContent("Walk Speed", "Determines how fast the player will move while walking."), fpc.walkSpeed, .1f, fpc.sprintSpeed);
-        GUI.enabled = true;
-
-        EditorGUILayout.Space();
-
-        #region Sprint
-
-        GUILayout.Label("Sprint", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleLeft, fontStyle = FontStyle.Bold, fontSize = 13 }, GUILayout.ExpandWidth(true));
-
-        fpc.enableSprint = EditorGUILayout.ToggleLeft(new GUIContent("Enable Sprint", "Determines if the player is allowed to sprint."), fpc.enableSprint);
-
-        GUI.enabled = fpc.enableSprint;
-        fpc.unlimitedSprint = EditorGUILayout.ToggleLeft(new GUIContent("Unlimited Sprint", "Determines if 'Sprint Duration' is enabled. Turning this on will allow for unlimited sprint."), fpc.unlimitedSprint);
-        fpc.sprintKey = (KeyCode)EditorGUILayout.EnumPopup(new GUIContent("Sprint Key", "Determines what key is used to sprint."), fpc.sprintKey);
-        fpc.sprintSpeed = EditorGUILayout.Slider(new GUIContent("Sprint Speed", "Determines how fast the player will move while sprinting."), fpc.sprintSpeed, fpc.walkSpeed, 20f);
-
-        //GUI.enabled = !fpc.unlimitedSprint;
-        fpc.sprintDuration = EditorGUILayout.Slider(new GUIContent("Sprint Duration", "Determines how long the player can sprint while unlimited sprint is disabled."), fpc.sprintDuration, 1f, 20f);
-        fpc.sprintCooldown = EditorGUILayout.Slider(new GUIContent("Sprint Cooldown", "Determines how long the recovery time is when the player runs out of sprint."), fpc.sprintCooldown, .1f, fpc.sprintDuration);
-        //GUI.enabled = true;
-
-        fpc.sprintFOV = EditorGUILayout.Slider(new GUIContent("Sprint FOV", "Determines the field of view the camera changes to while sprinting."), fpc.sprintFOV, fpc.fov, 179f);
-        fpc.sprintFOVStepTime = EditorGUILayout.Slider(new GUIContent("Step Time", "Determines how fast the FOV transitions while sprinting."), fpc.sprintFOVStepTime, .1f, 20f);
-
-        fpc.useSprintBar = EditorGUILayout.ToggleLeft(new GUIContent("Use Sprint Bar", "Determines if the default sprint bar will appear on screen."), fpc.useSprintBar);
-
-        // Only displays sprint bar options if sprint bar is enabled
-        if(fpc.useSprintBar)
-        {
-            EditorGUI.indentLevel++;
-
-            EditorGUILayout.BeginHorizontal();
-            fpc.hideBarWhenFull = EditorGUILayout.ToggleLeft(new GUIContent("Hide Full Bar", "Hides the sprint bar when sprint duration is full, and fades the bar in when sprinting. Disabling this will leave the bar on screen at all times when the sprint bar is enabled."), fpc.hideBarWhenFull);
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PrefixLabel(new GUIContent("Bar BG", "Object to be used as sprint bar background."));
-            fpc.sprintBarBG = (Image)EditorGUILayout.ObjectField(fpc.sprintBarBG, typeof(Image), true);
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PrefixLabel(new GUIContent("Bar", "Object to be used as sprint bar foreground."));
-            fpc.sprintBar = (Image)EditorGUILayout.ObjectField(fpc.sprintBar, typeof(Image), true);
-            EditorGUILayout.EndHorizontal();
-
-
-            EditorGUILayout.BeginHorizontal();
-            fpc.sprintBarWidthPercent = EditorGUILayout.Slider(new GUIContent("Bar Width", "Determines the width of the sprint bar."), fpc.sprintBarWidthPercent, .1f, .5f);
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-            fpc.sprintBarHeightPercent = EditorGUILayout.Slider(new GUIContent("Bar Height", "Determines the height of the sprint bar."), fpc.sprintBarHeightPercent, .001f, .025f);
-            EditorGUILayout.EndHorizontal();
-            EditorGUI.indentLevel--;
-        }
-        GUI.enabled = true;
-
-        EditorGUILayout.Space();
-
-        #endregion
-
-        #region Jump
-
-        GUILayout.Label("Jump", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleLeft, fontStyle = FontStyle.Bold, fontSize = 13 }, GUILayout.ExpandWidth(true));
-
-        fpc.enableJump = EditorGUILayout.ToggleLeft(new GUIContent("Enable Jump", "Determines if the player is allowed to jump."), fpc.enableJump);
-
-        GUI.enabled = fpc.enableJump;
-        fpc.jumpKey = (KeyCode)EditorGUILayout.EnumPopup(new GUIContent("Jump Key", "Determines what key is used to jump."), fpc.jumpKey);
-        fpc.jumpPower = EditorGUILayout.Slider(new GUIContent("Jump Power", "Determines how high the player will jump."), fpc.jumpPower, .1f, 20f);
-        GUI.enabled = true;
-
-        EditorGUILayout.Space();
-
-        #endregion
-
-        #region Crouch
-
-        GUILayout.Label("Crouch", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleLeft, fontStyle = FontStyle.Bold, fontSize = 13 }, GUILayout.ExpandWidth(true));
-
-        fpc.enableCrouch = EditorGUILayout.ToggleLeft(new GUIContent("Enable Crouch", "Determines if the player is allowed to crouch."), fpc.enableCrouch);
-
-        GUI.enabled = fpc.enableCrouch;
-        fpc.holdToCrouch = EditorGUILayout.ToggleLeft(new GUIContent("Hold To Crouch", "Requires the player to hold the crouch key instead if pressing to crouch and uncrouch."), fpc.holdToCrouch);
-        fpc.crouchKey = (KeyCode)EditorGUILayout.EnumPopup(new GUIContent("Crouch Key", "Determines what key is used to crouch."), fpc.crouchKey);
-        fpc.crouchHeight = EditorGUILayout.Slider(new GUIContent("Crouch Height", "Determines the y scale of the player object when crouched."), fpc.crouchHeight, .1f, 1);
-        fpc.speedReduction = EditorGUILayout.Slider(new GUIContent("Speed Reduction", "Determines the percent 'Walk Speed' is reduced by. 1 being no reduction, and .5 being half."), fpc.speedReduction, .1f, 1);
-        GUI.enabled = true;
-
-        #endregion
-
-        #endregion
-
-        #region Head Bob
-
-        EditorGUILayout.Space();
-        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-        GUILayout.Label("Head Bob Setup", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 13 }, GUILayout.ExpandWidth(true));
-        EditorGUILayout.Space();
-
-        fpc.enableHeadBob = EditorGUILayout.ToggleLeft(new GUIContent("Enable Head Bob", "Determines if the camera will bob while the player is walking."), fpc.enableHeadBob);
-        
-
-        GUI.enabled = fpc.enableHeadBob;
-        fpc.joint = (Transform)EditorGUILayout.ObjectField(new GUIContent("Camera Joint", "Joint object position is moved while head bob is active."), fpc.joint, typeof(Transform), true);
-        fpc.bobSpeed = EditorGUILayout.Slider(new GUIContent("Speed", "Determines how often a bob rotation is completed."), fpc.bobSpeed, 1, 20);
-        fpc.bobAmount = EditorGUILayout.Vector3Field(new GUIContent("Bob Amount", "Determines the amount the joint moves in both directions on every axes."), fpc.bobAmount);
-        GUI.enabled = true;
-
-        #endregion
-
-        //Sets any changes from the prefab
-        if(GUI.changed)
-        {
-            EditorUtility.SetDirty(fpc);
-            Undo.RecordObject(fpc, "FPC Change");
-            SerFPC.ApplyModifiedProperties();
-        }
     }
 
 }
